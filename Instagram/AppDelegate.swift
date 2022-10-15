24af36e22cee93fcc9cc2a752d738ab252e5fc06
Josephine Huang <johuan147@gmail.com>
2022-10-14 22:29:19 -0400

    Creating Instagram clone

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "byksCea7ZPHEjh8fJHzRf6L0vSnRXIu3lcnHcr1Q" // <- UPDATE
+                    $0.clientKey = "N1vehCsZNph7qhbKdPKjP5YP7xyeh1yNEjr9Bq8s" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
